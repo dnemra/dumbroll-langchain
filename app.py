935a63ce4eaec85d497acf857a0ca87cbe1228f0
dnemra <57673385+dnemra@users.noreply.github.com>
2025-07-23 18:50:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 import openai
 import os
